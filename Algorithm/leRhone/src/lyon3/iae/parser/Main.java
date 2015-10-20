503d907dbd7b600f89dc080b44933635d099524b
danielboni <danielboni@gmail.com>
2015-10-20 22:05:22 +0200

    relatorio

--- Algorithm/leRhone/src/lyon3/iae/parser/Main.java
+++ Algorithm/leRhone/src/lyon3/iae/parser/Main.java
@@ -15,9 +15,9 @@ public class Main {
 		rhone.createCSDs();
 		//rhone.print_CSDS();
 		rhone.combineCSDs();
-		rhone.print_permutations();
+		//rhone.print_permutations();
 		//rhone.printQuery();
-		//rhone.print_rewritings();
+		rhone.print_rewritings();
 	}
 }
  
