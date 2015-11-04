5efb9e47d657bf67c8d9c26d71fd3a6bdfbf99dc
danielboni <danielboni@gmail.com>
2015-11-04 11:55:11 +0100

    slides algo and tests

--- Algorithm/leRhone/src/lyon3/iae/parser/Main.java
+++ Algorithm/leRhone/src/lyon3/iae/parser/Main.java
@@ -1,5 +1,7 @@
 package lyon3.iae.parser;
 
+import java.lang.management.ManagementFactory;
+
 import org.dom4j.DocumentException;
 
 import lyon3.iae.rhone.Rhone;
@@ -7,17 +9,13 @@ import lyon3.iae.rhone.Rhone;
 public class Main {
 
 	public static void main(String[] args) throws DocumentException {
-		Rhone rhone = InputHandler.handleArguments("testcases.xml", "2");
-		rhone.printQuery();
-		rhone.printConcreteServices();
+		Rhone rhone = InputHandler.handleArguments("testcases1.xml", "4");
+		long start = ManagementFactory.getThreadMXBean().getCurrentThreadUserTime();
 		rhone.selectServices();
-		rhone.printCandidateServices();
-		//rhone.print_query_dependencies();
 		rhone.createCSDs();
-		rhone.print_CSDS();
 		rhone.combineCSDs();
-		//rhone.print_permutations();
-		//rhone.printQuery();
+        long time = ManagementFactory.getThreadMXBean().getCurrentThreadUserTime() - start;
+        System.out.println("Done in: " + time + "ns");
 		rhone.print_rewritings();
 	}
 }
