3e953ab65f752a32fac43cbd4874e775136cf144
danielboni <danielboni@gmail.com>
2016-01-19 13:54:00 +0100

    reproducing tests

--- Algorithm/leRhone/src/lyon3/iae/parser/Main.java
+++ Algorithm/leRhone/src/lyon3/iae/parser/Main.java
@@ -11,15 +11,8 @@ public class Main {
 
 	public static void main(String[] args) throws DocumentException {
 		
-//		Rhone rhone = InputHandler.handleArguments("testcases10.xml", "5");
-//		//long start = ManagementFactory.getThreadMXBean().getCurrentThreadUserTime();
-//		rhone.selectServices();
-//		rhone.createCSDs();
-//		rhone.combineCSDs();
-//		rhone.print_permutations();
-		
-		for (Integer i = 0; i < 8; i++) {
-			Rhone rhone = InputHandler.handleArguments("testcases1.xml", i.toString());
+		for (Integer i = 0; i < 6; i++) {
+			Rhone rhone = InputHandler.handleArguments("testcasesD.xml", i.toString());
 			long start = ManagementFactory.getThreadMXBean().getCurrentThreadUserTime();
 			rhone.selectServices();
 			//rhone.printCandidateServices();
@@ -30,10 +23,8 @@ public class Main {
 			//rhone.combineCSDs();
 			//rhone.print_permutations();
 			long time = ManagementFactory.getThreadMXBean().getCurrentThreadUserTime() - start;
-	        System.out.println("Testcase: " + i + " \tNumber of CSDs: " + rhone.getCsds().size() + "\tNumber of rewritings: " + rhone.getNumberOfRewritings() +  " \t--Done in: " + (double)time/1000000000 + " seconds");
-//	        System.out.println("Testcase: " + i + " \tNumber of CSDs: " + rhone.getCsds().size() + " \tNumber of services: " + rhone.concreteServices.size() +  " \t--Done in: " + (double)time/1000000000 + " seconds");
-//	        for (CSD csd: rhone.getCsds())
-//	        	System.out.println(csd.getConcrete_service().getName());
+	        System.out.println("Testcase: " + i + " \tNumber of CSDs: " + rhone.getCsds().size() + 
+	        		"\tNumber of rewritings: " + rhone.getNumberOfRewritings() +  " \t--Done in: " + (double)time/1000000000 + " seconds");
 	        System.gc();
 		}
 	}
