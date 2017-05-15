5434e4bbb53dc3fc8eab1fe8056e159046f60e43
danielboni <danielboni@gmail.com>
2017-05-15 15:05:39 +0200

    Organizando os experimentos e incluindo o do 3 algoritmo

---
+++ Algorithm/leRhone/src/lyon3/iae/datamodel/populate/experiment04/TestExperiment.java
@@ -0,0 +1,56 @@
+package lyon3.iae.datamodel.populate.experiment04;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.dom4j.DocumentException;
+
+import lyon3.iae.parser.InputHandler;
+import lyon3.iae.rhone.Rhone;
+
+public class TestExperiment {
+
+	public static void main(String[] args) throws DocumentException, SQLException, IOException {
+		
+		String resultFileName = "rewriting_2_500.txt";
+		File file = new File(resultFileName);
+		FileWriter writer = new FileWriter(file, true);
+		
+		writer.write("Results from rewriting queries with 2 abstract services and 500 matches.\n");
+
+		Rhone rhone = InputHandler.handleArguments("R-T9.xml", "0");
+
+		long tempInicial = System.currentTimeMillis();
+
+		rhone.selectCandidateServicesFromDB("query_history_12");
+//		System.out.println(rhone.getCadidateConcreteServices().size());
+
+		rhone.divideGroups2_NEW();
+		rhone.combine2_NEW();
+		
+		//			if (i == 1) {
+		//				rhone.divideGroups2_NEW();
+		//				rhone.combine2_NEW();
+		//			} else if (i == 2) {
+		//				rhone.divideGroups3_NEW();
+		//				rhone.combine3_NEW();
+		//			} else if (i == 3) {
+		//				rhone.divideGroups4_NEW();
+		//				rhone.combine4_NEW();	
+		//			} else if (i == 4) {
+		//				rhone.divideGroups5_NEW();
+		//				rhone.combine5_NEW();
+		//			}
+
+		long tempFinal = System.currentTimeMillis();
+		long dif = (tempFinal - tempInicial);
+
+		writer.write("Done in: \t" + dif + "\t Number of matchings " + rhone.getCadidateConcreteServices().size() + "\t Number of rewritings " + rhone.getCombinations().size());
+
+		writer.close();
+		System.out.println("Process Finished");
+	}
+
+}
---
+++ Algorithm/leRhone/src/lyon3/iae/datamodel/populate/experiment10/FeedingQueryHistory_3750_5_5.java
@@ -0,0 +1,326 @@
+package lyon3.iae.datamodel.populate.experiment10;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.List;
+
+import lyon3.iae.datamodel.ConcreteService;
+
+
+
+/*
+ * Autor: Daniel Aguiar
+ * 
+ * Descricao: Classe responsavel por alimentar o banco de dados com consultas.
+ * 
+ * */
+public class FeedingQueryHistory_3750_5_5 {
+	
+	public static void main(String[] args) {
+		FeedingQueryHistory_3750_5_5 feeding = new FeedingQueryHistory_3750_5_5();
+		feeding.populate("query_history_38");
+		feeding.populateJoinTable("query_history_38");
+	}
+	
+	public void populate(String databaseName) {
+		
+		String driverName = "com.mysql.jdbc.Driver";                        
+		 
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+
+		String serverName = "localhost";  
+		String mydatabase = databaseName;       
+		String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+
+		String username = "root";      
+		String password = "1234";
+
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+			
+			if (connection != null)
+				 System.out.println("Connected to the database.");
+			
+			System.out.println("Feeding the database with queries...");
+		
+//			System.out.println("0...");
+//			for (int i = 1; i < 10001; i++) {
+//				String sql1 = "insert into tb_query_history values (?, ?, ?, ?, "
+//						+ "'96', '1', '0.5', 'yes', 'yes', 'medium', 'low', 'reliable', 'working hours', '2', 'text', 'yes', "
+//						+ "'not certified', '5', '10');";
+//				
+//				PreparedStatement stmt = connection.prepareStatement(sql1);
+//				stmt.setInt(1, i);
+//				stmt.setString(2, "Processed");
+//		        stmt.setTimestamp(3, new Timestamp(new Date().getTime()));
+//		        stmt.setString(4, "Q"+i+"(x?; y!) := A1(x?; p!), A2(p?; y!)");
+//		        
+//				stmt.execute();
+//				stmt.close();
+//			}
+//			System.out.println("Incluindo 10.000 consultas com 3 abstratos...");
+//			for (int i = 1; i < 10001; i++) {
+//				String sql1 = "insert into tb_query_history values (?, ?, ?, ?, "
+//						+ "'96', '1', '0.5', 'yes', 'yes', 'medium', 'low', 'reliable', 'working hours', '2', 'text', 'yes', "
+//						+ "'not certified', '5', '10');";
+//				
+//				PreparedStatement stmt = connection.prepareStatement(sql1);
+//				stmt.setInt(1, i);
+//				stmt.setString(2, "Processed");
+//		        stmt.setTimestamp(3, new Timestamp(new Date().getTime()));
+//		        stmt.setString(4, "Q"+i+"(x?; y!, z!) := A1(x?; p!), A2(p?; y!), A3(p?; z!)");
+//		        
+//				stmt.execute();
+//				stmt.close();
+//			}
+//			System.out.println("Incluindo 10.000 consultas com 4 abstratos...");
+//			for (int i = 1; i < 10001; i++) {
+//				String sql1 = "insert into tb_query_history values (?, ?, ?, ?, "
+//						+ "'96', '1', '0.5', 'yes', 'yes', 'medium', 'low', 'reliable', 'working hours', '2', 'text', 'yes', "
+//						+ "'not certified', '5', '10');";
+//				
+//				PreparedStatement stmt = connection.prepareStatement(sql1);
+//				stmt.setInt(1, i);
+//				stmt.setString(2, "Processed");
+//		        stmt.setTimestamp(3, new Timestamp(new Date().getTime()));
+//		        stmt.setString(4, "Q"+i+"(x?, d?; y!, z!) := A1(x?; p!), A2(p?; y!), A3(p?; z!), A4(d?; p!)");
+//		        
+//				stmt.execute();
+//				stmt.close();
+//			}
+//			System.out.println("Incluindo 10.000 consultas com 6 abstratos...");
+//			for (int i = 1; i < 10001; i++) {
+//				String sql1 = "insert into tb_query_history values (?, ?, ?, ?, "
+//						+ "'96', '1', '0.5', 'yes', 'yes', 'medium', 'low', 'reliable', 'working hours', '2', 'text', 'yes', "
+//						+ "'not certified', '5', '10');";
+//				
+//				PreparedStatement stmt = connection.prepareStatement(sql1);
+//				stmt.setInt(1, i);
+//				stmt.setString(2, "Processed");
+//		        stmt.setTimestamp(3, new Timestamp(new Date().getTime()));
+//		        stmt.setString(4, "Q"+i+"(x?, d?; z!) := A1(x?; p!), A2(p?; z!), A3(p?; z!), A4(d?; p!), A5(d?; p!), A6(d?; p!)");
+//		        
+//				stmt.execute();
+//				stmt.close();
+//			}
+			System.out.println("Inserindo consultas com 5 serviços abstratos...");
+			for (int i = 1; i < 3751; i++) {
+				String sql1 = "insert into tb_query_history values (?, ?, ?, ?, "
+						+ "'96', '1', '0.5', 'yes', 'yes', 'medium', 'low', 'reliable', 'working hours', '2', 'text', 'yes', "
+						+ "'not certified', '5', '10');";
+				
+				PreparedStatement stmt = connection.prepareStatement(sql1);
+				stmt.setInt(1, i);
+				stmt.setString(2, "Processed");
+		        stmt.setTimestamp(3, new Timestamp(new Date().getTime()));
+		        stmt.setString(4, "Q"+i+"(x?,d?;z!):=A1(x?; p!),A2(p?; z!),A3(p?; z!),A4(d?; p!),A5(d?; p!)");
+		        
+				stmt.execute();
+				stmt.close();
+			}
+			
+			for (int i = 3751; i < 7501; i++) {
+				String sql1 = "insert into tb_query_history values (?, ?, ?, ?, "
+						+ "'96', '1', '0.5', 'yes', 'yes', 'medium', 'low', 'reliable', 'working hours', '2', 'text', 'yes', "
+						+ "'not certified', '5', '10');";
+				
+				PreparedStatement stmt = connection.prepareStatement(sql1);
+				stmt.setInt(1, i);
+				stmt.setString(2, "Processed");
+		        stmt.setTimestamp(3, new Timestamp(new Date().getTime()));
+		        stmt.setString(4, "Q"+i+"(x?,d?;z!):=A6(x?; p!),A7(p?; z!),A8(p?; z!),A9(d?; p!),A10(d?; p!)");
+		        
+				stmt.execute();
+				stmt.close();
+			}
+			
+			connection.close();
+	        
+	        System.out.println("Queries registered.");
+	        
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void populateJoinTable (String databaseName) {
+		
+		String driverName = "com.mysql.jdbc.Driver";                        
+		 
+		try {
+			Class.forName(driverName);
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+
+
+		String serverName = "localhost";  
+		String mydatabase = databaseName;       
+		String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+
+		String username = "root";      
+		String password = "1234";
+
+		try {
+			Connection connection = DriverManager.getConnection(url, username, password);
+			
+			if (connection != null)
+				 System.out.println("Connected to the database.");
+			
+			System.out.println("Feeding the database in the join table query/abstract services...");			
+	
+//			System.out.println("2 abstracts...");
+//			for (int i = 1; i < 10001; i++) {
+//				String sql1 = "insert into tb_query_abstract values (?, ?);";
+//				PreparedStatement stmt = connection.prepareStatement(sql1);
+//				stmt.setInt(1, i);
+//				for (int j = 1; j < 3; j++) {
+//					stmt.setInt(2, j);
+//					stmt.execute();
+//				}
+//				stmt.close();
+//				System.out.println("feito " + i);
+//			}
+//			
+//			System.out.println("3 abstracts...");
+//			for (int i = 10001; i < 20001; i++) {
+//				String sql1 = "insert into tb_query_abstract values (?, ?);";
+//				PreparedStatement stmt = connection.prepareStatement(sql1);
+//				stmt.setInt(1, i);
+//				for (int j = 1; j < 4; j++) {
+//					stmt.setInt(2, j);
+//					stmt.execute();
+//				}
+//				stmt.close();
+//				System.out.println("feito " + i);
+//			}
+//			
+//			System.out.println("4 abstracts...");
+//			for (int i = 20001; i < 30001; i++) {
+//				String sql1 = "insert into tb_query_abstract values (?, ?);";
+//				PreparedStatement stmt = connection.prepareStatement(sql1);
+//				stmt.setInt(1, i);
+//				for (int j = 1; j < 5; j++) {
+//					stmt.setInt(2, j);
+//					stmt.execute();
+//				}
+//				stmt.close();
+//				System.out.println("feito " + i);
+//			}
+//			
+//			System.out.println("5 abstracts...");
+//			for (int i = 30001; i < 40001; i++) {
+//				String sql1 = "insert into tb_query_abstract values (?, ?);";
+//				PreparedStatement stmt = connection.prepareStatement(sql1);
+//				stmt.setInt(1, i);
+//				for (int j = 1; j < 6; j++) {
+//					stmt.setInt(2, j);
+//					stmt.execute();
+//				}
+//				stmt.close();
+//				System.out.println("feito " + i);
+//			}
+			
+			System.out.println("Associando os serviços abstratos com as consultas de tamanho 5...");
+			for (int i = 1; i < 3751; i++) {
+				String sql1 = "insert into tb_query_abstract values (?, ?);";
+				PreparedStatement stmt = connection.prepareStatement(sql1);
+				stmt.setInt(1, i);
+				for (int j = 1; j < 6; j++) {
+					stmt.setInt(2, j);
+					stmt.execute();
+				}
+				stmt.close();
+				System.out.println("Feito " + i);
+			}
+			
+			for (int i = 3751; i < 7501; i++) {
+				String sql1 = "insert into tb_query_abstract values (?, ?);";
+				PreparedStatement stmt = connection.prepareStatement(sql1);
+				stmt.setInt(1, i);
+				for (int j = 6; j < 11; j++) {
+					stmt.setInt(2, j);
+					stmt.execute();
+				}
+				stmt.close();
+				System.out.println("Feito " + i);
+			}
+			
+			connection.close();
+	        
+	        System.out.println("Query/Abstract services registered.");
+	        
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void populateRewritings(int idQuery, List<List<ConcreteService>> combinations) throws SQLException{
+		int idComb = 1;
+		for (List<ConcreteService> list : combinations) {
+			PreparedStatement stmt = null;
+			Connection connection = null;
+			for (ConcreteService concreteService : list) {
+					
+				String driverName = "com.mysql.jdbc.Driver";                        
+				 
+				try {
+					Class.forName(driverName);
+				} catch (ClassNotFoundException e1) {
+					e1.printStackTrace();
+				}
+
+
+				String serverName = "localhost";  
+				String mydatabase = "query_history";       
+				String url = "jdbc:mysql://" + serverName + "/" + mydatabase;
+
+				String username = "root";      
+				String password = "1234";
+
+				try {
+					connection = DriverManager.getConnection(url, username, password);
+					
+					if (connection != null)
+						 System.out.println("Connected to the database.");
+					
+					System.out.println("Feeding the database combinations of data services...");			
+					
+					String sql = "insert into tb_combination values (?, ?);";
+					
+					stmt = connection.prepareStatement(sql);
+					stmt.setInt(1, idComb);
+					stmt.setInt(2, concreteService.getId());
+					stmt.execute();
+			        System.out.println("Combinations registered.");
+			        
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		
+			// Inserir na tabela rewritings
+			System.out.println("Feeding the database rewritings of the query...");	
+			String sql2 = "insert into tb_rewriting values (?, ?);";
+			
+			stmt = connection.prepareStatement(sql2);
+			stmt.setInt(1, idQuery);
+			stmt.setInt(2, idComb);
+			stmt.execute();
+	        System.out.println("Combinations registered.");
+			
+			stmt.close();
+	        connection.close();
+			idComb++;
+		}	
+	}
+}
