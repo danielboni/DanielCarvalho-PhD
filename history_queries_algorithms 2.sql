e0abaf0450ff61ac80c9cd0f3a31e67f6eae6f1d
danielboni <danielboni@gmail.com>
2017-06-07 13:28:19 +0200

    Atualizando: abordagem para reusabilidade/algoritmos

--- history_queries_algorithms 2.sql
+++ history_queries_algorithms 2.sql
@@ -60,6 +60,17 @@ and q.price_per_call <= 0.5 and (q.authentication = 'yes') and
 (q.veracity = 'reliable') and q.production_time = 'working hours' and q.production_rate <= 2.0 and (q.freshness = 'no' or q.freshness = 'yes') 
 and (q.provenance = 'not certified' or q.provenance = 'certified') and q.total_cost <= 10.0 and q.total_response_time <= 6.0 ;
 
+use query_history_11;
 
+select * from tb_query_history;
 
-select distinct q.id from tb_query_history q, tb_query_abstract a where q.id = a.id_query and ( a.id_abstract = 1 or a.id_abstract = 2 ) and q.availability <= 95.0 and q.response_time >= 2.0 and q.price_per_call >= 0.5 and (q.authentication = 'yes' or q.authentication = 'no') and (q.privacy = 'yes' or q.privacy = 'no') and (q.trust = 'low' or q.trust = 'medium') and q.degree_of_rawness = 'low' and (q.veracity = 'reliable' or q.veracity = 'unreliable') and q.production_time = 'working hours' and q.production_rate <= 2.0 and q.freshness = 'no' and q.provenance = 'not certified' and q.total_cost >= 10.0 and q.total_response_time >= 6.0 and ((select count(*) from tb_query_abstract a2 where a2.id_query = q.id) = 2);
+select distinct q.id from tb_query_history q, tb_query_abstract a where q.id = a.id_query and
+ q.availability <= 96.0 and q.response_time <= 2.0 and q.price_per_call <= 0.5 and
+ (q.authentication = 'yes' or q.authentication = 'no') and (q.privacy = 'yes' or q.privacy = 'no') and (q.trust = 'low' or q.trust = 'medium') and q.degree_of_rawness = 'low' and (q.veracity = 'reliable' or q.veracity = 'unreliable') and q.production_time = 'working hours' and q.production_rate <= 2.0 and q.freshness = 'yes' and q.provenance = 'not certified' and q.total_cost <= 10.0 and q.total_response_time >= 6.0 
+and q.id not in (select distinct qq.id from tb_query_history qq, tb_query_abstract aa where qq.id = aa.id_query and aa.id_abstract not in (1, 2, 3));
+
+select distinct qq.id from tb_query_history qq, tb_query_abstract aa where qq.id = aa.id_query and aa.id_abstract not in (1, 2, 3);
+
+select * from tb_query_abstract where id_query = 1;
+
+select * from tb_query_abstract a where a.id_query = 1;
